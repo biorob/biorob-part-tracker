84badc3c2ba923f8deb0689c7b874d8b26fb774a
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2012-11-17 10:54:28 +0100

    Add Setting class and example file

---
+++ config/initializers/1_settings.rb
@@ -0,0 +1,12 @@
+class Settings < Settingslogic
+  source "#{Rails.root}/config/biorob-part-tracker.yml"
+  namespace Rails.env
+
+  def ldap_enabled?
+    ldap && ldap['enabled']
+  rescue SettingsLogic::MissingSetting
+    false
+  end
+
+end
+
