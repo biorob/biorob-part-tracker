4c9e596089be130d4df7682fb14667682ca35d94
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2012-11-17 09:04:58 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BiorobPartTracker::Application.config.secret_token = 'f5c14e110c5af31741f644b488cc994783fa29cb421cfe42c4fe45c3c779bbbfa6110dc1c2b5f5ecce2ff3e84ada5989c1f2d6ac857fac9cb9aea1b74dc44c52'
